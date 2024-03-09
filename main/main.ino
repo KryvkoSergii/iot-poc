52da4b6e2cc5e0e0a3b318f951540e3876391b0d
Sergii <kryvkosergii@gmail.com>
2024-03-09 16:06:04 +0200

    tcp-server

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,4 @@
-
+#include <UIPEthernet.h>
 //pins for LED
 #define PIN_LED_1 2
 #define PIN_LED_2 3
@@ -23,6 +23,21 @@
 //Ethernet Serial Clock
 #define ETHERNET_SCK 13
 
+#define LISTENPORT 80
+#define MACADDRESS 0x00, 0x01, 0x02, 0x03, 0x04, 0x05
+#define MYIPADDR 192, 168, 1, 6
+#define MYIPMASK 255, 255, 255, 0
+#define MYDNS 192, 168, 1, 1
+#define MYGW 192, 168, 1, 1
+
+uint8_t mac[6] = { MACADDRESS };
+uint8_t myIP[4] = { MYIPADDR };
+uint8_t myMASK[4] = { MYIPMASK };
+uint8_t myDNS[4] = { MYDNS };
+uint8_t myGW[4] = { MYGW };
+
+EthernetServer server(LISTENPORT);
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pins as an output.
@@ -34,10 +49,26 @@ void setup() {
   // stepmotor initial values
   digitalWrite(PIN_STEPMOTOR_STEP, 1);
   digitalWrite(PIN_STEPMOTOR_DIR, 0);
+
+  // initialize the ethernet device
+  //Ethernet.begin(mac,myIP);
+  Ethernet.begin(mac, myIP, myDNS, myGW, myMASK);
+  // start listening for clients
+  server.begin();
 }
 
 
 void loop() {
+  if (EthernetClient client = server.available()) {
+    while ((size = client.available()) > 0) {
+      uint8_t* msg = (uint8_t*)malloc(size + 1);
+      memset(msg, 0, size + 1);
+      size = client.read(msg, size);
+      // here should be server logic
+
+      free(msg);
+    }
+  }
 }
 
 void turnOnLed(int led) {
